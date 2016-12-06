d9b675808b3fc0809a5d2069e7fe932697752e20
Curtis Decker <deckercu@msu.edu>
2016-12-06 01:09:25 -0500

    Add files via upload

---
+++ final/js/javascript.js
@@ -0,0 +1,8 @@
+function toggle(){
+    var div = document.getElementById("divSection");
+    if (div.style.display =='block'){
+        div.style.display = 'none';
+        return;
+    }
+    div.style.display ='block';
+}
